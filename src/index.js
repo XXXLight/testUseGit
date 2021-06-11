037269e1a280c49136e42c7c2c06e625aa7d215a
XXXLight <827128200@qq.com>
2021-06-11 19:31:04 +0800

    da-web组件test1版本

--- src/index.js
+++ src/index.js
@@ -1,17 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+
+class Square extends React.Component{
+  constructor(props){
+    super(props);
+    this.state={
+      value:null,
+    };
+    console.log("constro");
+  }
+  componentDidMount(){
+    console.log("componentDidMount");
+  }
+  componentDidUpdate(){
+    console.log("componentDidUpdate");
+  }
+  render(){
+    console.log("render");
+    return (
+      <button className="square" onClick={()=>this.setState({value:'X'})}>
+        {this.state.value}
+      </button>
+    );
+  }
+}
+
+
+// ========================================
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Square />,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
